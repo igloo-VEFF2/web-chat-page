3616fa476132da3d7c50b4e7e1b755256cbbd36a
Maciuska <Maciuska14@gmail.com>
2017-02-20 18:58:08 +0000

    Basic work

---
+++ client/src/app/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ChatService } from "../chat.service";
+import { Router } from "@angular/router";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  userName: string;
+  loginFailed: boolean;
+
+  constructor(private chatService : ChatService,
+    private router: Router) { 
+
+  }
+
+  ngOnInit() {
+  }
+
+    onLogin() {
+      this.chatService.login(this.userName).subscribe(succeded => {
+        this.loginFailed = !succeded;
+        if (succeded === true) {
+          this.router.navigate(["/rooms"]);
+        }
+      });
+
+      }
+
+}
